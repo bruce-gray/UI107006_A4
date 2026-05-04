fbec2f2b3e2945cf08f31180b0dcf79df1e148c7
bruce-gray <brucefledge@gmail.com>
2026-05-04 19:01:46 +0100

    Added Flask routes to save and delete quizzes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import json
 import os
+import uuid
 
 app = Flask(__name__)
 
@@ -112,6 +113,60 @@ def check_answer():
 @app.route('/create')
 def create():
     return app.send_static_file('create.html')
+
+# route for saving quizzes to quizzes.json
+@app.route('/api/quiz', methods=['POST'])
+def save_quiz():
+    data = request.get_json()
+
+    # error handling for bad request
+    if not data:
+        return jsonify({'error': 'No data received.'}), 400
+    
+    # store the receieved POST body
+    title = data.get('title')
+    description = data.get('description')
+    questions = data.get('questions')
+
+    # input validation, all fields must be entered
+    if not title or not description or not questions:
+        return jsonify({'error': 'Missing required fields.'}), 400
+    
+    quizzes = load_quizzes()
+    new_quiz = {
+        # uuid is a standard python library for generating unique identifiers
+        # uuid4 generates a completely random uuid
+        'id': str(uuid.uuid4()),
+        'title': title,
+        'description': description,
+        'questions': questions
+    }
+
+    # appends new_quiz to the full list and calls save_quizzes to overwrite old list with new list
+    quizzes.append(new_quiz)
+    save_quizzes(quizzes)
+
+    # return success confirmation message and 201 (code for Created)
+    return jsonify({'success': True, 'id': new_quiz['id']}), 201
+
+# receives the full list of quizzes and writes to quizzes.json
+def save_quizzes(quizzes):
+    with open(DATA_FILE, 'w') as f:
+        json.dump(quizzes, f, indent=4)
+
+# handles finding and deleting a specific quiz when requested
+@app.route('/api/quiz/<quiz_id>', methods=['DELETE'])
+def delete_quiz(quiz_id):
+    quizzes = load_quizzes()
+    # for each quiz in quizzes, if the quiz_id received does not match the quiz_id of the quiz, add it to updated. the quiz for deletion is not passed to this new list
+    updated = [quiz for quiz in quizzes if quiz['id'] != quiz_id]
+
+    # if the new list len is identical to the old len, it means the quiz was not found
+    if len(updated) == len(quizzes):
+        return jsonify({'error': 'Quiz not found.'}), 404
+    
+    save_quizzes(updated)
+    return jsonify({'success': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
